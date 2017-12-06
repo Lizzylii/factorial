0cf400290d1fb43ed694549b5ae78ec78e297b59
User <User@K3-512-02>
2017-12-06 20:49:38 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int Factorial(int n)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int factorial = 1;
+            bool ok = true;
+
+            do
+            {
+                Console.WriteLine("Введите целое число");
+                string buf = Console.ReadLine();
+                ok = int.TryParse(buf, out n);
+            } while (!ok);
+
+            factorial = Factorial(n);
+            Console.WriteLine("factorial = {0}", factorial);
+        }
+
+        public static int Factorial(int n)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
